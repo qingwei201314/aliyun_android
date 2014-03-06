59216053173a3fabd0aacba95bbdb44e375c8987
qingwei201314 <87863823@qq.com>
2014-03-06 18:21:03 +0800

    c

--- src/com/example/kevin/MainActivity.java
+++ src/com/example/kevin/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,9 +13,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
